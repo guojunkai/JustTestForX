9f5254ce785b032121b59f088a435dd7a3b37655
zhangqiang <1069507276@qq.com>
2017-03-03 17:03:57 +0800

    ....

---
+++ JustTestForX/JustTestForX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JustTestForX
+//
+//  Created by zhangqiang on 17/3/3.
+//  Copyright © 2017年 jyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JustTestForX/JustTestForX/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  JustTestForX
+//
+//  Created by zhangqiang on 17/3/3.
+//  Copyright © 2017年 jyfl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"test");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
